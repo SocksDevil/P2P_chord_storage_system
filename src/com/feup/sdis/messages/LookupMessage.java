bb4cf39cd622ac08cc93b30dacaddfa735775e1b
Manu <manuel.monge.coutinho@gmail.com>
2020-05-02 15:43:02 +0100

    Changed Replication table

--- src/com/feup/sdis/messages/LookupMessage.java
+++ src/com/feup/sdis/messages/LookupMessage.java
@@ -6,19 +6,21 @@ import com.feup.sdis.peer.Server;
 public class LookupMessage extends Message {
 
     private String chunkID;
+    private int currRepDegree;
     private SocketAddress addressInfo;
 
     // chunkID -> hash(fileName#chunkNo#repID)
-    public LookupMessage(String chunkID, SocketAddress addressInfo){
+    public LookupMessage(String chunkID, int currRepDegree, SocketAddress addressInfo){
 
         // TODO: if args != X throw IllegalArgumentException/MessageError...
         this.chunkID = chunkID;
+        this.currRepDegree = currRepDegree;
         this.addressInfo = addressInfo;
     }
 
     @Override
     public Message handle() {
-        this.addressInfo = Server.chord.getDest(this.addressInfo, this.chunkID);
+        this.addressInfo = Server.chord.getDest(this.addressInfo, this.chunkID, this.currRepDegree);
      
         if(this.addressInfo == null){
             System.out.println("TODO: An error occured on LookupMessage.handle");
